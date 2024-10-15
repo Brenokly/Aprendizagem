c3e54fb611990dbfec2460b3742b0182dd28e5dc
BrenoKly <breno.moura@launos.ufersa.edu>
2024-10-14 21:07:10 -0300

    components

--- Aulas-React/my-first-project/src/App.js
+++ Aulas-React/my-first-project/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const name = 'Breno'
@@ -17,6 +18,7 @@ function App() {
       <p>Olá, {nameCase}</p>
       <p>Soma: {sum(1,2)}</p>
       <img src={url} alt="Minha Imagem" />
+      <HelloWorld/>
     </div>
   );
 }
